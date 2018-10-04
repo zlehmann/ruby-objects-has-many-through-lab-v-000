f5f912ce76de2680f2196c8b188d24eadef52431
Zachary Lehmann <zlehmann@gmail.com>
2018-10-04 19:36:03 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,19 +3,19 @@ class Patient
   @@all = []
 
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 
   def new_appointment(doctor, date)
     appointment = Appointment.new(self, doctor, date)
   end
 
-  def appointments 
+  def appointments
     Appointment.all.select { |appt| appt.patient == self }
   end
 
